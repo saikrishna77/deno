2d4104f964e68253b4d6187aeab7f2e56bd5ed6a
saikrishna <samalakrishna7@gmail.com>
2020-11-27 18:15:59 +0530

    dg

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,33 +1,49 @@
-import React, { Component } from "react";
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AppContext } from './Context';
+import SignUp from './signup.component';
 
-export default class Login extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign In</h3>
+const Login = (props) => {
+  //   const [file, setFile] = useState('');
+  const context = useContext(AppContext);
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+  return (
+    <form>
+      <h3>Web Push</h3>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+      <div className='form-group'>
+        <label>YOUR REST API KEY</label>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='API Key'
+          onChange={(e) => {
+            context.setKey(e.target.value);
+          }}
+        />
+      </div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+      <div className='form-group'>
+        <label>YOUR AUTH KEY</label>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='AUTH Key'
+          onChange={(e) => {
+            context.setToken(e.target.value);
+          }}
+        />
+      </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
-        );
-    }
-}
+      <Link to='/Step2'>
+        <button type='submit' className='btn btn-primary btn-block'>
+          Submit
+        </button>
+      </Link>
+      <p className='forgot-password text-right'>
+        Forgot <a href='#'>password?</a>
+      </p>
+    </form>
+  );
+};
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import AppContextProvider from './components/Context';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById("root")
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
 
 serviceWorker.unregister();
