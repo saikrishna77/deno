c4349c5d0649aa88ba9acb5efacaed780340aaf7
saikrishna <samalakrishna7@gmail.com>
2021-01-24 17:28:31 +0530

    update 2

--- src/Component/ID/ID_Start.jsx
+++ src/Component/ID/ID_Start.jsx
@@ -3,7 +3,87 @@ import "../../App.css";
 const IDMaker = () => {
   return (
     <div class="">
-      <div></div>
+      <div>
+        <table>
+          <tr>
+            <td>
+              {" "}
+              <label>Account Type : </label>
+            </td>{" "}
+            <td>
+              <input type="text" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Account Number</label>
+            </td>
+            <td>
+              <input type="number" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Customer ID</label>
+            </td>
+            <td>
+              <input type="number" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Customer Name</label>
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>IFSC Code</label>
+            </td>
+            <td>
+              <input type="number" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Mobile Number</label>
+            </td>
+            <td>
+              <input type="number" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Branch Name</label>
+            </td>
+            <td>
+              <input type="text" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Customer Care Number</label>
+            </td>
+            <td>
+              <input type="number" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label>Upload Picture</label>
+            </td>
+            <td>
+              <input type="file" />
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <div></div>
+        <div>Image</div>
+      </div>
     </div>
   );
 };
